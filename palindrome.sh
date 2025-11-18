ec31312f0771c1a8abcf88cdaa58448ee529aa6a
yokesh <yokesh@localhost.localdomain>
2025-11-19 00:57:15 +0100

    Success

---
+++ palindrome.sh
@@ -0,0 +1,22 @@
+#num=545
+echo "Enter the number"
+read num
+s=0
+rev=""
+temp=$num
+while [ $num -gt 0 ]
+do
+	# Get Remainder
+	s=$(( $num % 10 ))
+	# Get next digit
+	num=$(( $num / 10 ))
+	# Store previous number and
+	# current digit in reverse
+	rev=$( echo ${rev}${s} )
+done
+if [ $temp -eq $rev ];
+then
+	echo "Number is palindrome"
+else
+	echo "Number is NOT palindrome"
+fi
